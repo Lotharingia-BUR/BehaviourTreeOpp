803cd099ece25ebf8d0b6323a10e4f929f2cfe45
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2025-05-09 12:39:11 -0400

    wwhy wont they collide :(
    HELP

---
+++ Assets/Scripts/C#/Statue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statue : MonoBehaviour
+{
+    public bool isTriggred = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(isTriggred);
+    }
+
+    private void OnTrigger(Collision collision)
+    {
+        isTriggred = true;
+    }
+}
