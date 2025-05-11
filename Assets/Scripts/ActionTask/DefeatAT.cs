de72b11f8269705a8916ed7178c4e71d98748413
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2025-05-11 16:40:35 -0400

    Fixed Death Bug

--- Assets/Scripts/ActionTask/DefeatAT.cs
+++ Assets/Scripts/ActionTask/DefeatAT.cs
@@ -18,8 +18,9 @@ namespace NodeCanvas.Tasks.Actions {
 		//This is called once each time the task is enabled.
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
-		protected override void OnExecute() { 
-		}
+		protected override void OnExecute() {
+            NavAgent.SetDestination(agent.transform.position);
+        }
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
